f9face7262066d77b3fc693cde676a7a9356b753
DoriaFabio <doriafabio2001@gmail.com>
2025-06-06 14:54:10 +0200

    add comment

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
+//! Funzione asincrona che effettua una richiesta HTTP a un URL e restituisce il JSON
 async function fetchJson(url) {
     const res = await fetch(url);
     const obj = await res.json();
     return obj;
 }
 
+//! Funzione che, dato l'id di una ricetta, restituisce la data di nascita dello chef che l'ha creata
 async function getChefBirthday(id) {
-    let post;
+    let ricetta;
     try {
-        post = await fetchJson(`https://dummyjson.com/recipes/${id}`);
-        console.log("Ricetta: ", post);
+        ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+        console.log("Ricetta: ", ricetta);
     } catch (error) {
         throw new Error(`Non posso recuperare la ricetta con id: ${id}`)
     }
-    if (post.message) {
-        throw new Error(post.message);
+    if (ricetta.message) {
+        throw new Error(ricetta.message);
     }
-    let user;
+    let chef;
     try {
-        user = await fetchJson(`https://dummyjson.com/users/${post.userId}`);
-        console.log("Chef: ", user);
+        chef = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`);
+        console.log("Chef: ", chef);
     } catch (error) {
-        throw new Error(`Non posso recuperare user id ${post.userId}`)
+        throw new Error(`Non posso recuperare user id ${ricetta.userId}`)
     }
-    if(user.message) {
-        throw new Error(user.message);
+    if(chef.message) {
+        throw new Error(chef.message);
     }
-    return user.birthDate;
+    return chef.birthDate;
 }
 
+//! Blocco di codice che si esegue subito (IIFE - Immediately Invoked Function Expression)
 (async () => {
     try {
         const birthDate = await getChefBirthday(5);
-        console.log(birthDate);
+        console.log("Data di nascita dello Chef:", birthDate);
     } catch (error) {
         console.error(error);
     } finally {
